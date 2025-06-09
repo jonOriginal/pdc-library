2771e6c5bccd0720e4729e34715e34c3faae343b
mervy24 <mervy24@eva00.local>
2025-06-09 13:31:08 +1200

    Wrote the SQL table creation

--- src/main/java/com/pdc/library/db/LibSql.java
+++ src/main/java/com/pdc/library/db/LibSql.java
@@ -3,11 +3,31 @@ package com.pdc.library.db;
 public class LibSql {
     private LibSql() {}
 
-    public static final String CREATE_BOOK_TABLE = "";
+    public static final String BOOK_TABLE_NAME = "Books";
+    public static final String USER_TABLE_NAME = "Users";
+    public static final String USER_BOOK_TABLE_NAME = "UserBooks";
 
-    public static final String CREATE_USER_TABLE = "";
+    public static final String CREATE_BOOK_TABLE = "CREATE TABLE IF NOT EXISTS " + BOOK_TABLE_NAME + " (" +
+                                                   "BookID int NOT NULL," +
+                                                   "BookAuthor var(255)," +
+                                                   "BookName var(255)," +
+                                                   "PRIMARY KEY (BookID)" +
+                                                   ");";
 
-    public static final String CREATE_USER_BOOK_TABLE = "";
+    public static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + USER_TABLE_NAME + " (" +
+                                                   "UserID int NOT NULL," +
+                                                   "UserName VAR(255)," +
+                                                   "PRIMARY KEY (BookID)" +
+                                                   ");";+
+
+    public static final String CREATE_USER_BOOK_TABLE = "CREATE TABLE IF NOT EXISTS " + USER_BOOK_TABLE_NAME + " (" +
+                                                        "UserID int," +
+                                                        "BookID int," +
+                                                        "DateHired date," +
+                                                        "PRIMARY KEY (UserID, BookID)," +
+                                                        "FOREIGN KEY (UserID) REFERENCES " + USER_TABLE_NAME + "(UserID)," +
+                                                        "FOREIGN KEY (BookID) REFERENCES " + BOOK_TABLE_NAME + "(BookID)" +
+                                                        ");";
 
     public static final String INSERT_BOOK = "";
 
