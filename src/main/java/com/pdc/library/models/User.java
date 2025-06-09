aa6bf1226a56ec0f64f1be154b8ea29924644372
mervy24 <mervy24@eva00.local>
2025-06-09 12:52:08 +1200

    Initialised database models

--- src/main/java/com/pdc/library/models/User.java
+++ src/main/java/com/pdc/library/models/User.java
@@ -1,4 +1,27 @@
 package com.pdc.library.models;
 
 public class User {
+    private int userID;
+    private String userName;
+
+    public User() {
+        this.userID = 0;
+        this.userName = "";
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int newUserID) {
+        this.userID = newUserID;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public void setUserName(String newUserName) {
+        this.userName = newUserName;
+    }
 }
--- src/main/java/com/pdc/library/models/UserBook.java
+++ src/main/java/com/pdc/library/models/UserBook.java
@@ -1,4 +1,22 @@
 package com.pdc.library.models;
 
+import java.time.LocalDate;
+
 public class UserBook {
+    private int userID;
+    private int bookID;
+    private LocalDate dateHired;
+
+    public UserBook(int userID, int bookID) {
+        this.userID = getUserID();
+        this.bookID = getBookID();
+    }
+    
+    public LocalDate getDateHired() {
+        return dateHired;
+    }
+
+    public void setDateHired(LocalDate newDateHired) {
+        this.dateHired = newDateHired;
+    }
 }
