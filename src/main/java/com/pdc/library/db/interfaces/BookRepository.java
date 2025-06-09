fb78fb72d097708a15cace063d677433ee256044
jonOriginal <jonathan-knox@outlook.com>
2025-06-10 11:19:54 +1200

    Adding throw exception to interfaces

--- src/main/java/com/pdc/library/db/interfaces/BookRepository.java
+++ src/main/java/com/pdc/library/db/interfaces/BookRepository.java
@@ -2,12 +2,15 @@ package com.pdc.library.db.interfaces;
 
 import com.pdc.library.models.Book;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 public interface BookRepository {
-    void addBook(Book book);
-    void removeBook(Book book);
+    void addBook(Book book) throws SQLException;
+    void removeBook(int bookId) throws SQLException;
     void updateBook(Book book);
+
+    Collection<Book> findAllBooks();
 
     Book findBookById(int id);
     Collection<Book> findBookByTitle(String title);
